4115abe51a1deede238259d53088a954a82883cd
manoj singhal <manoj841922@gmail.com>
2017-10-09 16:53:33 +0530

    config chanegs

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=MunchAdo',
-    'username' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=Mydb',
+    'username' => 'manoj',
     'password' => 'abc@123',
     'charset' => 'utf8',
 ];
