1e2bcd13aa267b5351c6df7467bdd2a57eb6edf0
ashokbhati <ashokbhati40@gmail.com>
2017-10-09 14:24:27 +0530

    initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=MunchAdo',
+    'username' => 'root',
+    'password' => 'abc@123',
+    'charset' => 'utf8',
+];
